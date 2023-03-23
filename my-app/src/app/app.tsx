879f934481bcb4f876e6053ee8ec9aa130f58858
Ming-der Wang <mingderwang@gmail.com>
2023-03-23 15:23:54 +0800

    add my-app, nx generate @nrwl/react:application my-app --framework=react --parcel

---
+++ my-app/src/app/app.tsx
@@ -0,0 +1,21 @@
+
+
+import NxWelcome from "./nx-welcome";
+
+
+
+export function App() {
+
+  return (
+    <>
+      
+      <NxWelcome title="my-app"/>
+      
+      <div />
+    </>);
+
+}
+
+
+export default App;
+
